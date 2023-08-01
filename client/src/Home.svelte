6818345242991aa3607d39ce69f4c2463b962b63
sourcherryscones <purva.marfatia@gmail.com>
2023-08-01 00:30:58 -0700

    tiny homepage edit

--- client/src/Home.svelte
+++ client/src/Home.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <main class="container">
-    <h1>Fluff loading...</h1>
-    <progress></progress>
+    <h1>Hello and welcome!</h1>
+    <p>Please register or log in to participate :)</p>
+    <!--<progress></progress>-->
 </main>
