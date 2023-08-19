148cf4840ba4c63f25a9ff74980459dac16098b9
sourcherryscones <purva.marfatia@gmail.com>
2023-08-18 21:17:31 -0700

    minor frontend things

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -12,7 +12,7 @@
           <li><a href="/register" use:link>Register</a></li>
           <li><a href="/login" use:link>Log In</a></li>
           <li><a href="/about" use:link>About</a></li>
-          <li><a href="/" use:link>Feed</a></li>
+          <li><a href="/feed" use:link>Feed</a></li>
           <li><a href="/myitems" use:link>My Items</a></li>
         </ul>
       </nav>
